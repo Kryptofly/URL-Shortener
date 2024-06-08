54eb1fb3bdc8bdb6bb5f4c8b0fcb45ab3f337844
Kryptofly <156387500+Kryptofly@users.noreply.github.com>
2024-06-09 02:54:41 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+import string
+import random
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///urls.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class URL(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    original_url = db.Column(db.String(500), nullable=False)
+    short_url = db.Column(db.String(10), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<URL {self.original_url}>'
+
+def generate_short_url():
+    characters = string.ascii_letters + string.digits
+    while True:
+        short_url = ''.join(random.choice(characters) for _ in range(6))
+        if not URL.query.filter_by(short_url=short_url).first():
+            return short_url
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        original_url = request.form['original_url']
+        if original_url:
+            short_url = generate_short_url()
+            new_url = URL(original_url=original_url, short_url=short_url)
+            db.session.add(new_url)
+            db.session.commit()
+            return render_template('index.html', short_url=short_url)
+    return render_template('index.html')
+
+@app.route('/<short_url>')
+def redirect_to_url(short_url):
+    url = URL.query.filter_by(short_url=short_url).first_or_404()
+    return redirect(url.original_url)
+
+if __name__ == '__main__':
+    app.run(debug=True)
